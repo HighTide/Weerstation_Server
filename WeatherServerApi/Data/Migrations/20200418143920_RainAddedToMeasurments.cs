1b776b7f7aa0119d27a374e9c1791d1a2baf09a5
Jurre Koetse <IO@Hightide.dev>
2020-04-18 16:40:13 +0200

    CORNE YOU FORGOT TO RUN MIGRATIONS

---
+++ WeatherServerApi/Data/Migrations/20200418143920_RainAddedToMeasurments.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeatherServerApi.Data.Migrations
+{
+    public partial class RainAddedToMeasurments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "RainSpeed",
+                table: "Measurements",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RainSpeed",
+                table: "Measurements");
+        }
+    }
+}
