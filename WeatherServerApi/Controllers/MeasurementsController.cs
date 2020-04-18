24226b5527563ec2296568d4df116e160abb1ce9
ARCIDEAN <0952113@hr.nl>
2020-04-18 13:20:05 +0200

    Added RainSpeed
    @hightide zoals jij de luchtdruk meet, ik ga de snelheid van neerslag berekenen. Ik heb hem voor je geïmplementeerd in deze branch.

--- WeatherServerApi/Controllers/MeasurementsController.cs
+++ WeatherServerApi/Controllers/MeasurementsController.cs
@@ -26,6 +26,7 @@ namespace WeatherServerApi.Controllers
             public decimal Temperature { get; set; }
             public decimal Humidity { get; set; }
             public decimal WindSpeed { get; set; }
+            public decimal RainSpeed { get; set; }
             public decimal Pressure { get; set; }
             public long Station { get; set; }
         }
@@ -42,6 +43,7 @@ namespace WeatherServerApi.Controllers
                 Temperature = x.Temperature,
                 Humidity = x.Humidity,
                 WindSpeed = x.WindSpeed,
+                RainSpeed = x.RainSpeed,
                 Pressure = x.Pressure,
                 Station = x.Station.Id,
             }).ToListAsync();
@@ -66,6 +68,7 @@ namespace WeatherServerApi.Controllers
                     Temperature = x.Temperature,
                     Humidity = x.Humidity,
                     WindSpeed = x.WindSpeed,
+                    RainSpeed = x.RainSpeed,
                     Pressure = x.Pressure,
                     Station = x.Station.Id,
                 }).FirstOrDefaultAsync();
