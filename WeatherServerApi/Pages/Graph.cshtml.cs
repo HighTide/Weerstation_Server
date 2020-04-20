450c9427b92476b8ba44afe01e2b622c6154f90c
frederiq <0941526@hr.nl>
2020-04-20 23:58:43 +0200

    Implemented charts

---
+++ WeatherServerApi/Pages/Graph.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WeatherServerApi.Data;
+
+namespace WeatherServerApi
+{
+    public class GraphModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GraphModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Station> stations { get; set; }
+
+        public void OnGet()
+        {
+            stations = _context.Stations.ToList();
+        }
+    }
+}
